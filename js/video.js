7fb9ca294f2190ec12eb10460b29180f4ca78575
ryanbaxter99 <73867292+ryanbaxter99@users.noreply.github.com>
2022-04-04 22:17:43 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,87 @@
+var video = document.querySelector("#player1")
+
+// load
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.autoplay = false;
+	video.loop = false;
+});
+
+// play button
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.getElementById('volume').innerHTML = video.volume * 100 + "%";
+});
+
+// pausing the video
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+// slower
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slowing down");
+	console.log("Current speed is " + video.playbackRate);
+	video.playbackRate *= 0.95;
+	console.log("New speed is " + video.playbackRate);
+});
+
+// faster 
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speeding up");
+	console.log("Current speed is " + video.playbackRate);
+	video.playbackRate /= 0.95;
+	console.log("New speed is " + video.playbackRate);
+});
+
+// skip
+document.querySelector("#skip").addEventListener("click", function() {
+
+	console.log("Original Location: " + video.currentTime);
+
+	// if th currnt time + 15 is greater then the video length go back to 0
+	if (video.currentTime + 15 > video.duration) {
+		video.currentTime = 0;
+		console.log("Video restarted")
+	} 
+	// if not then just add 15 
+	else {
+		video.currentTime += 15;
+		
+	}
+	console.log("New Location: " + video.currentTime);
+});
+
+// Mute
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("The value of mute is " + video.muted)
+
+	if (video.muted == true) {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+	else {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+});
+
+// volume slider 
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log(this.value);
+	video.volume = this.value / 100; 
+	document.querySelector("#volume").innerHTML = this.value + "%";
+});
+
+// styling
+document.querySelector("#vintage").addEventListener("click", function() {
+	document.querySelector("video").classList.add("oldSchool");
+	console.log(video.classList);
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	document.querySelector("video").classList.remove("oldSchool");
+	console.log(videp.classList);
+});
